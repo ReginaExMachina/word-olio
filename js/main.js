14f4645aa63b80c4be41d7ac59f5e1cd75a874d4
Rachel <regexina@gmail.com>
2017-08-25 16:28:22 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Dictionary
+
+var wordsList = [
+  "amorous: 1: inclined toward or displaying love 2: expressive of or exciting sexual love or romance",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  ""
+  "",
+  "",
+  ""
+];
